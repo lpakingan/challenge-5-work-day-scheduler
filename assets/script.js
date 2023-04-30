668c8690079162b3bbbb0b5869714d3678d23496
lpakingan <lpak98@gmail.com>
2023-04-30 08:32:43 -0700

    add comments to js file

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,49 @@
+// function runs on initial page load in
 $(function () {
 
+  // uses dayjs to find the current date/time and writes it to the scheduler page
+  $('#currentDay').text(dayjs().format('[Today is ] dddd, MMMM DD[th.]'));
+
+  function currentTime() {
+    var time = dayjs().format('[The time right now is] hh:mm:ss a[.]');
+    $('#currentTime').text(time);
+  }
+
+  // setInterval allows for dynamic showing of the current time
+  currentTime();
+  setInterval(currentTime, 1000);
+  
+  // sets the time-block variable (where all the timeblocks are children of) and logs the current hour
+  var timeblockEl = $('.time-block');
+  currentHour24 = dayjs().format('H');
+  currentHour = parseInt(currentHour24);
+  currentHour12 = dayjs().format('hA')
+  console.log(`The current hour is ${currentHour12} in 12hr or ${currentHour24} in 24hr time formats`);
+
+  // for each hourly time-block, the timeblock's hour is sliced and compared to the current hour
+  $.each(timeblockEl, function () {
+    var timeblockID = $(this).attr('id');
+    var timeblockHour = timeblockID.slice(5);
+
+  // if the timeblock hour is less than the current hour, adds the class 'past'
+  if (timeblockHour < currentHour) {
+    $(this).addClass('past');
+    // $(this).removeClass('present');
+    // $(this).removeClass('future');
+    //if the timeblock hour is equal to the current hour, adds the class 'present'
+  } else if (timeblockHour == currentHour) {
+    // $(this).removeClass('past');
+    $(this).addClass('present');
+    // $(this).removeClass('future');
+    // if the timeblock hour is greater than the current hour, adds the class 'future'
+  } else {
+    // $(this).removeClass('past');
+    // $(this).removeClass('present');
+    $(this).addClass('future');
+  }});
+
+  // gives functionality to the save button by using the 'this' variable to store the timeblockID and user input
+  // if the user input is empty the user is alerted they need to input again
   $('.saveBtn').on('click', function() {
     var hourID = $(this).parent().attr('id');
     var userInput = $(this).parent().children().eq(1).val().trim();
@@ -11,31 +55,7 @@ $(function () {
       console.log(hourID, userInput)};
   })
 
-  var timeblockEl = $('.time-block');
-  currentHour24 = dayjs().format('H');
-  currentHour = parseInt(currentHour24);
-  currentHour12 = dayjs().format('hA')
-  console.log(`The current hour is ${currentHour12} in 12hr or ${currentHour24} in 24hr time formats`);
-
-  $.each(timeblockEl, function () {
-    var timeblockID = $(this).attr('id');
-    var timeblockHour = timeblockID.slice(5);
-
-  // removeClass not needed?
-  if (timeblockHour < currentHour) {
-    $(this).addClass('past');
-    // $(this).removeClass('present');
-    // $(this).removeClass('future');
-  } else if (timeblockHour == currentHour) {
-    // $(this).removeClass('past');
-    $(this).addClass('present');
-    // $(this).removeClass('future');
-  } else {
-    // $(this).removeClass('past');
-    // $(this).removeClass('present');
-    $(this).addClass('future');
-  }});
-
+  // gets the locally stored inputs and sets the value of the corresponding ID's description as the user input
   hour8Input = localStorage.getItem('hour-8');
   $('#hour-8 .description').val(hour8Input);
   hour9Input = localStorage.getItem('hour-9');
@@ -56,14 +76,5 @@ $(function () {
   $('#hour-16 .description').val(hour16Input);
   hour17Input = localStorage.getItem('hour-17');
   $('#hour-17 .description').val(hour17Input);
-
-  $('#currentDay').text(dayjs().format('[Today is ] dddd, MMMM DD[th.]'));
-
-  function currentTime() {
-    var time = dayjs().format('[The time right now is] hh:mm:ss a[.]');
-    $('#currentTime').text(time);
-  }
-
-  currentTime();
-  setInterval(currentTime, 1000);
 });
+
