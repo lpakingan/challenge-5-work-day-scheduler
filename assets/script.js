fe82ffe3ef7880e60df0392ee3897effdbbe6168
lpakingan <lpak98@gmail.com>
2023-04-30 08:20:41 -0700

    add function to show current time on webpage

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,10 @@ $(function () {
     var hourID = $(this).parent().attr('id');
     var userInput = $(this).parent().children().eq(1).val().trim();
     if (userInput == null || userInput == '') {
-      alert('You must enter valid input!')
-      console.log(hourID, userInput)
+      alert('You must enter valid input!');
+      console.log(hourID, userInput);
     } else {
-      localStorage.setItem(hourID, userInput)
+      localStorage.setItem(hourID, userInput);
       console.log(hourID, userInput)};
   })
 
@@ -53,28 +53,35 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  hour8Input = localStorage.getItem('hour-8')
-  $('#hour-8 .description').val(hour8Input)
-  hour9Input = localStorage.getItem('hour-9')
-  $('#hour-9 .description').val(hour9Input)
-  hour10Input = localStorage.getItem('hour-10')
-  $('#hour-10 .description').val(hour10Input)
-  hour11Input = localStorage.getItem('hour-11')
-  $('#hour-11 .description').val(hour11Input)
-  hour12Input = localStorage.getItem('hour-12')
-  $('#hour-12 .description').val(hour12Input)
-  hour13Input = localStorage.getItem('hour-13')
-  $('#hour-13 .description').val(hour13Input)
-  hour14Input = localStorage.getItem('hour-14')
-  $('#hour-14 .description').val(hour14Input)
-  hour15Input = localStorage.getItem('hour-15')
-  $('#hour-15 .description').val(hour15Input)
-  hour16Input = localStorage.getItem('hour-16')
-  $('#hour-16 .description').val(hour16Input)
-  hour17Input = localStorage.getItem('hour-17')
-  $('#hour-17 .description').val(hour17Input)
+  hour8Input = localStorage.getItem('hour-8');
+  $('#hour-8 .description').val(hour8Input);
+  hour9Input = localStorage.getItem('hour-9');
+  $('#hour-9 .description').val(hour9Input);
+  hour10Input = localStorage.getItem('hour-10');
+  $('#hour-10 .description').val(hour10Input);
+  hour11Input = localStorage.getItem('hour-11');
+  $('#hour-11 .description').val(hour11Input);
+  hour12Input = localStorage.getItem('hour-12');
+  $('#hour-12 .description').val(hour12Input);
+  hour13Input = localStorage.getItem('hour-13');
+  $('#hour-13 .description').val(hour13Input);
+  hour14Input = localStorage.getItem('hour-14');
+  $('#hour-14 .description').val(hour14Input);
+  hour15Input = localStorage.getItem('hour-15');
+  $('#hour-15 .description').val(hour15Input);
+  hour16Input = localStorage.getItem('hour-16');
+  $('#hour-16 .description').val(hour16Input);
+  hour17Input = localStorage.getItem('hour-17');
+  $('#hour-17 .description').val(hour17Input);
 
   // TODO: Add code to display the current date in the header of the page.
-  $('#currentDay').text(dayjs().format('dddd, MMMM DD[th]'))
-  
+  $('#currentDay').text(dayjs().format('[Today is ] dddd, MMMM DD[th.]'));
+
+  function currentTime() {
+    var time = dayjs().format('[The time right now is] hh:mm:ss a[.]');
+    $('#currentTime').text(time);
+  }
+
+  currentTime();
+  setInterval(currentTime, 1000);
 });
